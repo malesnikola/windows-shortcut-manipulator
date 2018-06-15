ec54a62a72f2ef8a186ff148f35ea6c210a5d139
Nikola Malesevic <males.nikola@live.com>
2018-06-15 11:15:46 +0200

    - start implementing import files - add translations for all UI elements

--- src/main/java/controllers/MainScreenController.java
+++ src/main/java/controllers/MainScreenController.java
@@ -22,7 +22,12 @@ import javafx.util.Callback;
 import main.java.dialogs.ProgressForm;
 import main.java.domain.FailedFileDetails;
 import main.java.domain.WindowsShortcutDetails;
+import main.java.domain.WindowsShortcutWrapper;
+import main.java.enums.FileState;
+import main.java.enums.ShortcutActionState;
 import main.java.model.WindowsShortcutModel;
+import main.java.util.Constants;
+import main.java.workers.ImportFilesWorker;
 import org.apache.log4j.Logger;
 
 import java.io.File;
@@ -34,6 +39,8 @@ public class MainScreenController implements WindowsShortcutModel.WindowsShortcu
     private static Logger logger = Logger.getLogger(MainScreenController.class);
 
     private static ResourceBundle resourceBundle;
+
+    private WindowsShortcutModel windowsShortcutModel;
 
     private Scene scene;
     private String chosenLanguage; // en (English), rs (Serbian)
@@ -82,6 +89,8 @@ public class MainScreenController implements WindowsShortcutModel.WindowsShortcu
     private Button createCopiesButton;
     @FXML
     private Button chooseDirectoryButton;
+    @FXML
+    private Button clearConsoleButton;
 
     // choice box
     @FXML
@@ -115,10 +124,8 @@ public class MainScreenController implements WindowsShortcutModel.WindowsShortcu
     private TableColumn<WindowsShortcutDetails, String> shortcutFilePathColumn;
     @FXML
     private TableColumn<WindowsShortcutDetails, String> originalFilePathColumn;
-    @FXML
-    private TableColumn<WindowsShortcutDetails, String> availabilityColumn;
-    @FXML
-    private TableColumn<WindowsShortcutDetails, String> actionColumn;
+    private TableColumn availabilityColumn;
+    private TableColumn actionColumn;
 
     /**
      * Get resource bundle of application.
@@ -167,15 +174,194 @@ public class MainScreenController implements WindowsShortcutModel.WindowsShortcu
         directoryLabel.setText(getLocalizedString("label.directory.text"));
         consoleLabel.setText(getLocalizedString("label.console.text"));
 
+        // buttons
+        checkAvailabilityButton.setText(getLocalizedString("button.checkAvailability.text"));
+        checkDuplicatesButton.setText(getLocalizedString("button.checkDuplicates.text"));
+        changeRootButton.setText(getLocalizedString("button.changeRoot.text"));
+        createCopiesButton.setText(getLocalizedString("button.createCopies.text"));
+        chooseDirectoryButton.setText(getLocalizedString("button.chooseDirectory.text"));
+        clearConsoleButton.setTooltip(new Tooltip(getLocalizedString("button.clearConsole.tooltip")));
+
+        // data table
+        tableView.setPlaceholder(new Label(getLocalizedString("table.placeholder.text")));
+
+        // columns in data table
+        shortcutFilePathColumn.setText(getLocalizedString("table.column.shortcutFilePath.text"));
+        originalFilePathColumn.setText(getLocalizedString("table.column.originalFilePath.text"));
+        availabilityColumn.setText(getLocalizedString("table.column.availability.text"));
+        actionColumn.setText(getLocalizedString("table.column.action.text"));
     }
 
     @FXML
     public void initialize() {
-        // by default, languague is English
+        windowsShortcutModel = WindowsShortcutModel.getInstance();
+        windowsShortcutModel.registerObserver(this);
+
+        // by default, language is English
         resourceBundle = ResourceBundle.getBundle("bundles.Bundle", new Locale("en", "EN"));
         chosenLanguage = "en";
         englishMenuItem.setSelected(true);
+
+        // enable multiple selection in table
+        tableView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        // add key event handling for deleting files and for clearing selection
+        tableView.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            switch (event.getCode()) {
+                case DELETE:
+                    ObservableList<WindowsShortcutDetails> selectedItems = tableView.getSelectionModel().getSelectedItems();
+                    List<String> filePathsForRemove = selectedItems
+                            .stream()
+                            .map(WindowsShortcutDetails::getShortcutFilePath)
+                            .collect(Collectors.toList());
+                    windowsShortcutModel.removeImportedFiles(filePathsForRemove);
+                    break;
+                case ESCAPE:
+                    tableView.getSelectionModel().clearSelection();
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        // add mouse event handling for clearing selection on empty row click
+        tableView.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            Node source = event.getPickResult().getIntersectedNode();
+
+            // move up through the node hierarchy until a TableRow or scene root is found
+            while (source != null && !(source instanceof TableRow)) {
+                source = source.getParent();
+            }
+
+            // clear selection on click anywhere but on a filled row
+            if (source == null || (source instanceof TableRow && ((TableRow) source).isEmpty())) {
+                tableView.getSelectionModel().clearSelection();
+            }
+        });
+
+        // save user selected column for sorting
+        tableView.setOnSort(event -> {
+            if (tableView.getSortOrder().size() == 1) {
+                chosenSortingColumn = tableView.getSortOrder().get(0);
+            }
+        });
+
+        // by default sorting column is file name
+        shortcutFilePathColumn.setSortType(TableColumn.SortType.ASCENDING);
+        chosenSortingColumn = shortcutFilePathColumn;
+
+        initNewTableColumn();
+
         populateUIWithLocalizedStrings();
+    }
+
+    private void initNewTableColumn() {
+        availabilityColumn = new TableColumn(getLocalizedString("table.column.availability.text"));
+        availabilityColumn.setPrefWidth(100);
+        availabilityColumn.setCellValueFactory(new PropertyValueFactory<WindowsShortcutDetails,String>("availability"));
+
+        // add cell factory for coloring states:
+        //   To obtain the TableCell we need to replace the Default CellFactory with one that returns a new TableCell instance,
+        //   and @Override the updateItem(String item, boolean empty) method.
+        availabilityColumn.setCellFactory(new Callback<TableColumn, TableCell>() {
+            public TableCell call(TableColumn param) {
+                return new TableCell<WindowsShortcutDetails, String>() {
+
+                    @Override
+                    public void updateItem(String item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (!isEmpty()) {
+                            FileState state = FileState.fromString(item);
+
+                            switch (state) {
+                                case UNKNOWN:
+                                    this.setTextFill(Color.BLACK);
+                                    break;
+                                case AVAILABLE:
+                                    this.setTextFill(Color.GREEN);
+                                    break;
+                                case UNAVAILABLE:
+                                    this.setTextFill(Color.RED);
+                                    break;
+                            }
+
+                            setText(item);
+                            this.setStyle("-fx-alignment: CENTER;");
+                        } else {
+                            setText("");
+                        }
+                    }
+                };
+            }
+        });
+
+        actionColumn = new TableColumn(getLocalizedString("table.column.action.text"));
+        actionColumn.setPrefWidth(100);
+        actionColumn.setCellValueFactory(new PropertyValueFactory<WindowsShortcutDetails,String>("lastAction"));
+
+        // add cell factory for coloring states:
+        //   To obtain the TableCell we need to replace the Default CellFactory with one that returns a new TableCell instance,
+        //   and @Override the updateItem(String item, boolean empty) method.
+        actionColumn.setCellFactory(new Callback<TableColumn, TableCell>() {
+            public TableCell call(TableColumn param) {
+                return new TableCell<WindowsShortcutDetails, String>() {
+
+                    @Override
+                    public void updateItem(String item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (!isEmpty()) {
+                            ShortcutActionState state = ShortcutActionState.fromString(item);
+
+                            switch (state) {
+                                case NONE:
+                                    this.setTextFill(Color.BLACK);
+                                    break;
+                                case SAVED:
+                                    this.setTextFill(Color.GREEN);
+                                    break;
+                                case MODIFIED:
+                                    this.setTextFill(Color.GREEN);
+                                    break;
+                                case FAILED_MODIFIED:
+                                    this.setTextFill(Color.RED);
+                                    break;
+                                case FAILED_SAVED:
+                                    this.setTextFill(Color.RED);
+                                    break;
+                            }
+
+                            setText(item);
+                            this.setStyle("-fx-alignment: CENTER;");
+                        } else {
+                            setText("");
+                        }
+                    }
+                };
+            }
+        });
+
+        // add availability column
+        tableView.getColumns().addAll(availabilityColumn, actionColumn);
+    }
+
+    /**
+     * Update table view with data.
+     */
+    private void updateTable() {
+        tableData = FXCollections.observableArrayList();
+        for (WindowsShortcutWrapper shortcutFile : windowsShortcutModel.getImportedFiles().values()) {
+            tableData.add(WindowsShortcutDetails.deserialize(shortcutFile));
+        }
+
+        shortcutFilePathColumn.setCellValueFactory(new PropertyValueFactory<>("shortcutFilePath"));
+        originalFilePathColumn.setCellValueFactory(new PropertyValueFactory<>("originalFilePath"));
+
+        tableView.setItems(null);
+        tableView.setItems(tableData);
+
+        // sort table
+        tableView.getSortOrder().add(chosenSortingColumn);
+        tableView.sort();
     }
 
     /**
@@ -190,7 +376,7 @@ public class MainScreenController implements WindowsShortcutModel.WindowsShortcu
             @Override
             public void handle(DragEvent event) {
                 Dragboard db = event.getDragboard();
-                // if at least one of the dragged files has ".mp3" extension enable importing
+                // if there is at least one of the dragged files
                 if (db.hasFiles()) {
                     event.acceptTransferModes(TransferMode.COPY);
                 } else {
@@ -200,35 +386,35 @@ public class MainScreenController implements WindowsShortcutModel.WindowsShortcu
         });
 
         // add drop over event handling
-//        scene.setOnDragDropped(new EventHandler<DragEvent>() {
-//            @Override
-//            public void handle(DragEvent event) {
-//                Dragboard db = event.getDragboard();
-//                boolean success = false;
-//                if (db.hasFiles()) {
-//                    success = true;
-//                    List<File> dbFiles = db.getFiles();
-//
-//                    ProgressForm progressForm = new ProgressForm(scene);
-//                    Task importFilesWorker = new ImportFilesWorker(mp3Model, progressForm, dbFiles);
-//
-//                    // binds progress of progress form to progress of task
-//                    progressForm.activateProgressBar(importFilesWorker);
-//
-//                    // disable all elements on scene
-//                    scene.getRoot().getChildrenUnmodifiable().forEach(c -> c.setDisable(true));
-//
-//                    // open progress dialog
-//                    progressForm.getDialogStage().show();
-//
-//                    // saveFiles new thread
-//                    new Thread(importFilesWorker).start();
-//                }
-//
-//                event.setDropCompleted(success);
-//                event.consume();
-//            }
-//        });
+        scene.setOnDragDropped(new EventHandler<DragEvent>() {
+            @Override
+            public void handle(DragEvent event) {
+                Dragboard db = event.getDragboard();
+                boolean success = false;
+                if (db.hasFiles()) {
+                    success = true;
+                    List<File> dbFiles = db.getFiles();
+
+                    ProgressForm progressForm = new ProgressForm(scene);
+                    Task importFilesWorker = new ImportFilesWorker(windowsShortcutModel, progressForm, dbFiles);
+
+                    // binds progress of progress form to progress of task
+                    progressForm.activateProgressBar(importFilesWorker);
+
+                    // disable all elements on scene
+                    scene.getRoot().getChildrenUnmodifiable().forEach(c -> c.setDisable(true));
+
+                    // open progress dialog
+                    progressForm.getDialogStage().show();
+
+                    // saveFiles new thread
+                    new Thread(importFilesWorker).start();
+                }
+
+                event.setDropCompleted(success);
+                event.consume();
+            }
+        });
     }
 
     /**
@@ -326,7 +512,28 @@ public class MainScreenController implements WindowsShortcutModel.WindowsShortcu
      * Open file chooser dialog and import selected files into mp3Model.
      */
     public void openFiles() {
+        FileChooser fileChooser = new FileChooser();
+        // set extension filter
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(Constants.LNK_FILE_TYPE_DESCRIPTION, "*" + Constants.LNK_FILE_TYPE_EXTENSION);
+        fileChooser.getExtensionFilters().add(extFilter);
+        // Open dialog for choosing mp3 files
+        List<File> files = fileChooser.showOpenMultipleDialog(tableView.getScene().getWindow());
+        if (files != null) {
+            ProgressForm progressForm = new ProgressForm(scene);
+            Task importFilesWorker = new ImportFilesWorker(windowsShortcutModel, progressForm, files);
 
+            // binds progress of progress form to progress of task
+            progressForm.activateProgressBar(importFilesWorker);
+
+            // disable all elements on scene
+            scene.getRoot().getChildrenUnmodifiable().forEach(c -> c.setDisable(true));
+
+            // open progress dialog
+            progressForm.getDialogStage().show();
+
+            // saveFiles new thread
+            new Thread(importFilesWorker).start();
+        }
     }
 
     /**
@@ -338,7 +545,10 @@ public class MainScreenController implements WindowsShortcutModel.WindowsShortcu
 
     @Override
     public void onImportedFilesChanged() {
-
+        Platform.runLater(() -> {
+            updateTable();
+            addInfoOnConsole();
+        });
     }
 
     @Override
--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -1,8 +1,10 @@
 package main.java.util;
 
 public class Constants {
-    public static final String APP_VERSION = "2018.04.06";
+    public static final String APP_VERSION = "2018.06.15";
 
+    public static final String LNK_FILE_TYPE_EXTENSION = ".lnk";
+    public static final String LNK_FILE_TYPE_DESCRIPTION = "Windows shortcut files (*.lnk)";
     public static final String MP3_FILE_TYPE_EXTENSION = ".mp3";
     public static final String MP3_FILE_TYPE_DESCRIPTION = "MP3 files (*.mp3)";
 }
